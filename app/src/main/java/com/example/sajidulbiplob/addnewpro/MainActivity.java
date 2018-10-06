f757ba15c1e39e07223f69324a863f85192bfeed
sajidulbiplob <biplobsajidul@gmail.com>
2018-10-06 15:44:15 +0600

    ok
    ok
    
    Co-Authored-By: sajidulbiplob <sajidulbiplob@users.noreply.github.com>

---
+++ app/src/main/java/com/example/sajidulbiplob/addnewpro/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.sajidulbiplob.addnewpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+    String addId = "ca-app-pub-3940256099942544~3347511713";
+    String addIdFahim = "ca-app-pub-7740466603679309~2701306225";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this,addIdFahim);
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
